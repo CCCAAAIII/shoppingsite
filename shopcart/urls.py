3ad5eee8312528a5087e1dc97cca174ff430d3da
caicai <328974233@qq.com>
2018-12-26 11:40:57 +0800

    '完成购物车商品的删除'

--- shopcart/urls.py
+++ shopcart/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import url,include
 
 from shopcart import views
 urlpatterns = [
+    # 购物车详情
     url(r'^mycart/$',views.index,name='mycart'),
+    # 删除购物车item
+    url(r'^deletecartitem/$', views.deleteitem, name='deleteitem'),
 
 ]
--- shopcart/views.py
+++ shopcart/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import CartItem
 from shop.models import Goods,GoodType
+from django.http import JsonResponse
 
 # Create your views here.
 def index(request):
@@ -10,3 +11,13 @@ def index(request):
     else:
         cart = CartItem.objects.filter(user_id=user_id)
         return render(request, 'shopcart/shoppingcart.html', {'cart': cart})
+def deleteitem(request):
+    id = request.GET.get('id')
+    try:
+        # print(id,type(id))
+        item = CartItem.objects.get(id=id)
+        # print(item)
+        item.delete()
+        return JsonResponse({'msg':'ok'})
+    except Exception as e:
+        return JsonResponse({'msg':'error'})
